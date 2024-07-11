9d3c0433d5a2780a28113a9ba7319267cec0f1b7
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2024-07-11 13:33:47 -0400

    Sync - 2024-07-11 13:33:47

--- .hammerspoon/spotify.lua
+++ .hammerspoon/spotify.lua
@@ -3,3 +3,12 @@ local chords = require "chords"
 hs.hotkey.bind({"ctrl"}, "p", hs.spotify.playpause)
 hs.hotkey.bind(chords.cmd_ctrl, "p", hs.spotify.previous)
 hs.hotkey.bind(chords.cmd_ctrl, "n", hs.spotify.next)
+
+local function sendSystemKey(key)
+  hs.eventtap.event.newSystemKeyEvent(key, true):post()
+  hs.eventtap.event.newSystemKeyEvent(key, false):post()
+end
+
+hs.hotkey.bind(chords.cmd_ctrl, "m", function() sendSystemKey("MUTE") end)
+hs.hotkey.bind(chords.cmd_ctrl, "-", function() sendSystemKey("SOUND_DOWN") end)
+hs.hotkey.bind(chords.cmd_ctrl, "=", function() sendSystemKey("SOUND_UP") end)
