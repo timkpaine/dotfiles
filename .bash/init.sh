ec1a5aa2acb9ef516ee2e92c96ad28d25425c415
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-05-26 23:05:54 -0400

    .

--- .bash/init.sh
+++ .bash/init.sh
@@ -6,8 +6,8 @@ if [ "$(uname)" == "Darwin" ]; then
   source /opt/homebrew/Cellar/lunchy/0.10.4/etc/bash_completion.d/lunchy-completion.bash
 
   # fzf
-  source /opt/homebrew/Cellar/fzf/0.61.3/shell/completion.bash
-  source /opt/homebrew/Cellar/fzf/0.61.3/shell/key-bindings.bash
+  source /opt/homebrew/Cellar/fzf/0.62.0/shell/completion.bash
+  source /opt/homebrew/Cellar/fzf/0.62.0/shell/key-bindings.bash
 
   # nvm
   export NVM_DIR="$HOME/.nvm"
